9d0e70f2b8cd255af97449bfe419a35ac799a0a3
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-27 19:16:06 +0530

    Added api endpoint for basic user details

--- api/authentication/urls.py
+++ api/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 # from authentication import views
-from .views import RegisterUser, LoginUser, LogoutUser
+from .views import RegisterUser, LoginUser, LogoutUser, GetDetails
 
 urlpatterns = [
     path('register/', RegisterUser),
     path('login/', LoginUser),
     path('logout/', LogoutUser),
+    path('get_basic_details/', GetDetails),
 ]
--- api/authentication/views.py
+++ api/authentication/views.py
@@ -5,6 +5,22 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from rest_framework.decorators import api_view
 
+
+def CurrentUser(request):
+    if not request.user.is_authenticated:
+        return None
+    return request.user
+
+@api_view(['GET'])
+def GetDetails(request):
+    if request.method == 'GET':
+        usr = CurrentUser(request)
+        if usr == None:
+            return Response({"success":True, "loggedin": False}, status=status.HTTP_200_OK)
+        return Response({"success":True, "loggedin": True, **dict(UserSerializer(usr).data)})
+    return Response({"success":False, "message": "Make a GET request."}, status=status.HTTP_400_BAD_REQUEST)
+
+
 @api_view(['POST'])
 def RegisterUser(request):
     if request.method == 'POST':
@@ -14,31 +30,33 @@ def RegisterUser(request):
             uname = form.cleaned_data.get('username')
             pwd = form.cleaned_data.get('password')
             user = authenticate(username=uname, password=pwd)
+            logout(request)
             login(request, user)
             serializer = UserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({"message": "Invalid credentials."}, status=status.HTTP_406_NOT_ACCEPTABLE)
-    return Response({"message": "Make a POST request."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"success":True, **dict(serializer.data)})
+        return Response({"success":False, "message": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
+    return Response({"success":False, "message": "Make a POST request."}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def LoginUser(request):
     if request.method == 'POST':
+        logout(request)
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             serializer = UserSerializer(user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
-    return Response({"message": "Make a POST request."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"success":True, **dict(serializer.data)}, status=status.HTTP_200_OK)
+        return Response({"success":False, "message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+    return Response({"success":False, "message": "Make a POST request."}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 def LogoutUser(request):
     if request.method == 'GET':
         try :
             logout(request)
-            return Response({"message": "Successfully logged out."}, status=status.HTTP_200_OK)
+            return Response({"success":True, "message": "Successfully logged out."}, status=status.HTTP_200_OK)
         except:
-            return Response({"message": "Some error occurred. Try again later."}, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"message": "Make a GET request."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"success":False, "message": "Some error occurred. Try again later."}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"success":False, "message": "Make a GET request."}, status=status.HTTP_400_BAD_REQUEST)
