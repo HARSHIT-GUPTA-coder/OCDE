f56467a3ce5c358bffc1a930edd977d9ea0ccf46
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-27 18:23:09 +0530

    update api/urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,17 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from authentication import views
-
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include('authentication.urls')),
     path('compile', include('compilation.urls')),
 ]
+
