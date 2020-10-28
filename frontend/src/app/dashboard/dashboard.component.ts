ba7f3bad85fc3e200380dea0a2fad81cd4e1a3bb
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-28 14:23:37 +0530

    Added logout service to dashboard component

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { User } from '../UserDetails';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private router: Router,
+              private api: ApiService) { }
 
   ngOnInit(): void {
+  }
+
+  logout() {
+    this.api.logoutUser().subscribe(res => {
+      this.user = res;
+      if (this.user.success) {
+        this.router.navigate(['home']);
+      }
+    });
   }
 
 }
