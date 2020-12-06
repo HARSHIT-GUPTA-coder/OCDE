69b30b4a9974c0563f206e3876135b8cfe889804
Harshit Gupta <mail4harshit1@gmail.com>
2020-12-07 02:01:19 +0530

    add scroll

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,10 +8,6 @@ declare var particlesJS: any;
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  myStyle: object = {};
-  myParams: object = {};
-  width: number = 100;
-  height: number = 100;
   constructor() { }
 
   ngOnInit(): void {
@@ -20,30 +16,9 @@ export class HomeComponent implements OnInit {
       let el = document.querySelector(".particles-js-canvas-el"); 
       // el.setAttribute("height", "100%");
     });
-    this.myStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
-      'z-index': -1,
-      'top': 0,
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
-  };
-
-this.myParams = {
-      particles: {
-          number: {
-              value: 200,
-          },
-          color: {
-              value: '#ff0000'
-          },
-          shape: {
-              type: 'triangle',
-          },
-  }
-};
   }
 
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior:"smooth"});
+}
 }
