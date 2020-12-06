fa670b80a4f6e53d263fc42fafcb07f83c1e2f32
akj0811 <jain.ankit201108@gmail.com>
2020-12-06 15:04:03 +0530

    New user works perfect

--- api/new_user.sh
+++ api/new_user.sh
@@ -2,11 +2,11 @@
 
 mkdir -p "./files/${1}"
 mkdir -p "./files2/${1}"
+mkdir -p "./files2/${1}/${1}"
+mount --bind -o ro "./files/${1}" "./files2/${1}/${1}"
 
 for ch in "bin" "lib" "lib64" "usr"
 do 
     mkdir -p "./files2/${1}/${ch}"
-    mkdir -p "./files2/projects"
     mount --bind -o ro "/${ch}" "./files2/${1}/${ch}"
-    mount --bind -o ro "./files/${1}" "./files2/projects"
 done
